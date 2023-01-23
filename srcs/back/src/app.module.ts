9921928f6b301707b1b9abae3258814c82cf445e
Loan Matarise <lmataris@c2r2p2.42nice.fr>
2023-01-23 16:05:31 +0100

    fdsfsd

--- srcs/back/src/app.module.ts
+++ srcs/back/src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './database/user.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'toto',
+      password: 'toto',
+      database: 'transcendence',
+      entities: [User],
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
